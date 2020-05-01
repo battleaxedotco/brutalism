4610d9db8b88ad113375d7056b0ac2c620f28291
Tom Scharstein <trscharstein@gmail.com>
2020-04-30 21:29:58 -0700

    Verbose classes and styles for Anno

--- components/Anno/Anno.vue
+++ components/Anno/Anno.vue
@@ -1,21 +1,37 @@
 <template>
-  <span :class="[ 'anno-wrapper', { uppercase } ]" :style="{
+  <span :class="[ 'anno-wrapper', { uppercase, italic, bold } ]" :style="{
     color: color,
     'font-size': size,
     'font-family': font,
-    'letter-spacing': letterSpacing,
+    'letter-spacing': computedSpacing,
     margin: margin,
-    'display': 'block'
+    'display': 'block',
+    'font-weight': computedWeight,
+    'word-spacing': wordSpacing,
+    'word-break': wordBreak
   }">
-    <span v-if="$slots.default" class="anno-content">
+    <span v-if="$slots.default" 
+      :style="{
+        'decoration': textDecoration
+      }"
+      :class="['anno-content', { strikethrough, overline, underline }]"
+    >
       <slot />
     </span>
-    <span v-else-if="text" class="anno-content">{{text}}</span>
+    <span v-else-if="text" :class="['anno-content', { strikethrough, overline, underline }]">{{text}}</span>
   </span>
 </template>
 
 <script>
 export default {
+  computed: {
+    computedWeight() {
+      return this.bold ? 'bold' : this.weight      
+    },
+    computedSpacing() {
+      return this.uppercase && this.letterSpacing == 'normal' ? '.25ch' : this.letterSpacing 
+    }
+  },
   props: {
     text: {
       type: String,
@@ -44,17 +60,55 @@ export default {
     margin: {
       type: String,
       default: '0px 0px 1ch 0px'
+    },
+    italic: {
+      type: Boolean,
+      default: false
+    },
+    bold: {
+      type: Boolean,
+      default: false
+    },
+    weight: {
+      type: String,
+      default: '500'
+    },
+    wordSpacing: {
+      type: String,
+      default: 'normal'
+    },
+    wordBreak: {
+      type: String,
+      default: 'normal'
+    },
+    underline: {
+      type: Boolean,
+      default: false
+    },
+    overline: {
+      type: Boolean,
+      default: false
+    },
+    textDecoration: {
+      type: String,
+      default: ''
+    },
+    strikethrough: {
+      type: Boolean,
+      default: false
     }
   }
 }
 </script>
 
 <style>
-.anno-wrapper {
-  margin-bottom: 1ch;
-  display: block;
-}
 .uppercase {
   text-transform: uppercase;
+}
+.italic {
+  font-style: italic;
+}
+.strikethrough {
+  text-decoration: line-through;
 }
 </style>
