01e21643f20a698a808326a619250d9ef26c9503
Tom Scharstein <trscharstein@gmail.com>
2020-05-02 16:49:25 -0700

    Adding props to Row

--- components/Row/Row.vue
+++ components/Row/Row.vue
@@ -1,18 +1,39 @@
 <template>
-	<div class="row" :style="{ margin: margin, padding: padding }">
-		<slot></slot>
+	<div 
+		class="row" 
+		:style="{ 
+			margin: margin, 
+			padding: padding,
+			justifyContent: justify,
+			alignItems: align
+		}"
+	>
+		<slot />
 	</div>
 </template>
 
 <script>
 export default {
-	mixins: [require("../mixinStyleProps").default]
+	mixins: [require("../mixinStyleProps").default],
+	props: {
+		justify: {
+			type: String,
+			default: 'flex-start',
+		},
+		align: {
+			type: String,
+			default: 'space-around'
+		},
+		wrap: {
+			type: String,
+			default: 'wrap'
+		}
+	}
 };
 </script>
 
 <style>
 .row {
-	/* background: rgba(90, 186, 230, 0.3); */
 	display: flex;
 	flex-direction: row;
 	justify-content: start;
