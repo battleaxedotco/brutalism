3922c5741a5a35b6bcfc7f2d6da0fb7260bee7ec
Tom Scharstein <trscharstein@gmail.com>
2020-05-02 20:47:02 -0700

    Expanding Divider props and usage

--- components/Divider/Divider.vue
+++ components/Divider/Divider.vue
@@ -1,5 +1,11 @@
 <template>
-	<div class="divider" :style="getStyle()"></div>
+	<div 
+		:class="['divider-line', {
+			vertical,
+			alt
+		}]" 
+		:style="getStyle()" 
+	/>
 </template>
 
 <script>
@@ -8,23 +14,88 @@ export default {
 		vertical: {
 			type: Boolean,
 			default: false
+		},
+		debug: {
+			type: Boolean,
+			default: false
+		},
+		alt: {
+			type: Boolean,
+			default: false
+		},
+		color: {
+			type: String,
+			default: ''
+		},
+		thickness: {
+			type: String,
+			default: '2px'
+		},
+		margin: {
+			type: String,
+			default: ''
+		},
+		padding: {
+			type: String,
+			default: '0px 3px'
 		}
 	},
-	mixins: [require("../mixinStyleProps").default],
 	methods: {
 		getStyle() {
-			return `
-				border-width: ${this.vertical ? "1px 0px 0px 0px" : "0px 1px 0px 0px"};
-				margin: 0px ${this.padding.length ? this.padding : "3"}px;
-			`;
+			let style = '';
+
+			if (this.vertical) {
+				style = `
+					width: ${this.thickness};
+					height: auto;
+				`
+				if (this.margin.length) style += `margin: ${margin};`
+			} else {
+				style = `
+					width: ${this.vertical ? this.thickness : '100%'};
+					height: ${this.vertical ? '100%' : this.thickness};
+					padding: ${this.padding};
+				`
+				if (this.margin.length) style += `margin: ${margin};`
+			}
+			if (this.color.length) style += `background-color: ${this.color};`
+			
+			return style
 		}
+	},
+	mounted() {
+		if (this.debug)
+			console.log('Hello')
 	}
 };
 </script>
 
 <style>
-.divider {
-	border-style: solid;
-	border-color: var(--divider-light);
+
+.divider-line {
+	box-sizing: border-box;
+	max-width: 100%;
+}
+
+.divider-line:not(.vertical) {
+	margin: 6px 0px;
+}
+
+.divider-line.vertical {
+	box-sizing: border-box;
+	height: 100%;
+	margin-left: 2px;
+	margin-right: 6px;
+}
+
+.wrapper > .divider-line {
+	margin-bottom: 14px;
+}
+
+.divider-line:not(.alt) {
+	background-color: var(--divider-light);
+}
+.divider-line.alt {
+	background-color: var(--divider-dark);
 }
 </style>
