300abac3f1df51d79c5bf07915512528dc2805fb
Tom Scharstein <trscharstein@gmail.com>
2020-07-24 11:07:30 -0700

    Fixing console JSX statements typo, verifying gehenna integration

--- components/Panel/Panel.vue
+++ components/Panel/Panel.vue
@@ -103,13 +103,13 @@ export default {
     if (window.__adobe_cep__) {
       this.csInterface = new CSInterface();
       this.csInterface.addEventListener("console", (msg) => {
-        console.log(msg.data);
+        if (msg.data && msg.data !== "undefined") console.log(msg.data);
       });
       this.csInterface.addEventListener("console_error", (msg) => {
-        console.error(msg.data);
+        if (msg.data && msg.data !== "undefined") console.error(msg.data);
       });
       this.csInterface.addEventListener("console_debug", (msg) => {
-        console.debug(msg.data);
+        if (msg.data && msg.data !== "undefined") console.debug(msg.data);
       });
       this.csInterface.addEventListener("console_clear", () => {
         console.clear();
